348e4b39eb8db8afd318f3c96bfa01b313d5b2ea
Kris Albert Lee <krisalbert@gmail.com>
2016-04-27 22:17:44 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var randomPerson = {
+  name: 'Kris',
+  email: 'kris.lee@hackreactor.com',
+  phone: 123-345-6789
+};
+
+var sayHello = function(person) {
+  console.log('Hello ' + person.name + '!');
+};
+
+var printEmail = function(person) {
+  console.log('email: ' + person.email);
+};
